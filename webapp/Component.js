307c660e03c38b78ce5fa8355954ddf0e828b56e
mcparrad <mc.parrad@uniandes.edu.co>
2022-05-14 11:16:47 -0500

    App done

---
+++ webapp/Component.js
@@ -0,0 +1,4 @@
+sap.ui.define(['sap/suite/ui/generic/template/lib/AppComponent'], function(AppComponent) { return AppComponent.extend("productsmanager.Component", {
+    metadata: { manifest: "json"
+    } });
+    });
